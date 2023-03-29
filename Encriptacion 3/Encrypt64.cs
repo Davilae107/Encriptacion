5320393cdd7369daf0a5c3cc71c40217963f3944
davilae107 <davilae107@gmail.com>
2023-03-29 17:30:38 -0600

    first commit

---
+++ Encriptacion 3/Encrypt64.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encriptacion_3
+{
+    internal class Encrypt64
+    {
+        public string Encriptar(string mensaje)
+        {
+            string result = string.Empty;
+            byte[] encrypted = System.Text.Encoding.Unicode.GetBytes(mensaje);
+            result = Convert.ToBase64String(encrypted);
+            return result;
+        }
+
+        public string Desencriptar(string mensajeENC)
+        {
+            string result = string.Empty;
+            byte[] decrypted = Convert.FromBase64String(mensajeENC);
+            result = System.Text.Encoding.Unicode.GetString(decrypted);
+            return result;
+        }
+
+    }
+}
---
+++ Encriptacion 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encriptacion_3
+{
+    class Program
+    {
+
+        static void Main(string[]args)
+        { 
+            Encrypt64 encr = new Encrypt64();
+                     
+            
+            Console.WriteLine("Escribe mensaje: ");
+            string pass = Console.ReadLine().ToString();
+            pass = encr.Encriptar(pass);
+
+            Console.WriteLine("El mensaje encriptado es: " + pass);
+       
+            Console.WriteLine("El mensaje desencriptado es: " + encr.Desencriptar(pass));
+
+            Console.Read();
+        }
+        
+        
+    }
+}
